91481ee386f6a11b8106648f1796b45383bfc97f
Lars Müller <mryellowhd@gmail.com>
2017-08-07 11:13:15 +0200

    Initial commit

---
+++ src/main/java/de/nomizon/lobby/main/Lobby.java
@@ -0,0 +1,21 @@
+package de.nomizon.lobby.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Erstellt von Lars am 07.08.2017.
+ * Alle Rechte vorbehalten.
+ * Dieser Code ist ausschließlich für Nomizon.de gedacht.
+ */
+public class Lobby extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
